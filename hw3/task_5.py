df9aa83ca62cafa643a688bef1fa132d58805724
Eugene <marat@iMac-Marat.local>
2019-05-07 17:18:31 +0300

    up

--- hw3/task_5.py
+++ hw3/task_5.py
@@ -5,12 +5,13 @@
 matrix = []
 
 for i in range(5):
-    array = [int(input(f'{j} элемент {i} массива ')) for j in range(3)]
+    array = [int(input(f'Введите {j} элемент {i} массива: ')) for j in range(3)]
     matrix.append(array)
-    for line in matrix:
-        sum_line = 0
-        for item in line:
-            sum_line += item
+
+for line in matrix:
+    sum_line = 0
+    for item in line:
+        sum_line += item
     line.append(sum_line)
 
 for line in matrix:
